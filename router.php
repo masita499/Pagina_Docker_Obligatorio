cf890f832dc6b9a214ac2930967cfd166c3f0f3c
Maximo <maxibrassetti@gmail.com>
2025-11-21 22:08:42 -0300

    actualizacion de db.php en vinculacion con RDS V5

--- router.php
+++ router.php
@@ -32,32 +32,43 @@
 //     }
 // }
 
-static public function submit()
-{
-    // Si viene ?uri=shop, usamos eso
-    if (isset($_GET['uri']) && $_GET['uri'] !== '') {
-        $uri = '/' . trim($_GET['uri'], '/');
-    } else {
-        // Si no, usamos la ruta tal cual
-        $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
-        if ($uri === '') {
-            $uri = '/';
-        }
+class Route {
+    private static $uriList = array();
+    private static $uriCallback = array();
+
+    static public function add($uri, $function)
+    {
+        self::$uriList[] = $uri;
+        self::$uriCallback[$uri] = $function;
     }
 
-    $doesUriMatch = false;
-
-    foreach (self::$uriList as $u) {
-        if ($u == $uri) {
-            $doesUriMatch = true;
-            break;
+    static public function submit()
+    {
+        // Si viene ?uri=shop, usamos eso
+        if (isset($_GET['uri']) && $_GET['uri'] !== '') {
+            $uri = '/' . trim($_GET['uri'], '/');
+        } else {
+            // Si no, usamos la ruta tal cual
+            $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
+            if ($uri === '') {
+                $uri = '/';
+            }
         }
-    }
 
-    if ($doesUriMatch) {
-        call_user_func(self::$uriCallback[$uri]);
-    } else {
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
+        $doesUriMatch = false;
+
+        foreach (self::$uriList as $u) {
+            if ($u == $uri) {
+                $doesUriMatch = true;
+                break;
+            }
+        }
+
+        if ($doesUriMatch) {
+            call_user_func(self::$uriCallback[$uri]);
+        } else {
+            http_response_code(404);
+            require __DIR__ . '/views/404.php';
+        }
     }
 }
