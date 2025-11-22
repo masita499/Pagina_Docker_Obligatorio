1572ed4c06492ced9ade04e29a9bf32f1af12054
Maximo <maxibrassetti@gmail.com>
2025-11-21 23:36:59 -0300

    actualizacion de router.php V1

--- router.php
+++ router.php
@@ -1,38 +1,6 @@
 <?php
 
-// class Route{
-//     private static $uriList = array();
-//     private static $uriCallback = array();
-
-//     static public function add($uri, $function)
-//     {
-//         self::$uriList[] = $uri;
-//         self::$uriCallback[$uri] = $function;
-//     }
-
-//     static public function submit()
-//     {
-//         $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
-//         $doesUriMatch = false;
-
-//         foreach(self::$uriList as $u)
-//         {
-//             if($u == $uri) {
-//                 $doesUriMatch = true;
-//                 break;
-//             }
-//         }
-
-//         if($doesUriMatch) {
-//             call_user_func(self::$uriCallback[$uri]);
-//         } else {
-//             http_response_code(404);
-//             require __DIR__ . '/views/404.php';
-//         }
-//     }
-// }
-
-class Route {
+class Route{
     private static $uriList = array();
     private static $uriCallback = array();
 
@@ -44,27 +12,18 @@ class Route {
 
     static public function submit()
     {
-        // Si viene ?uri=shop, usamos eso
-        if (isset($_GET['uri']) && $_GET['uri'] !== '') {
-            $uri = '/' . trim($_GET['uri'], '/');
-        } else {
-            // Si no, usamos la ruta tal cual
-            $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
-            if ($uri === '') {
-                $uri = '/';
-            }
-        }
-
+        $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
         $doesUriMatch = false;
 
-        foreach (self::$uriList as $u) {
-            if ($u == $uri) {
+        foreach(self::$uriList as $u)
+        {
+            if($u == $uri) {
                 $doesUriMatch = true;
                 break;
             }
         }
 
-        if ($doesUriMatch) {
+        if($doesUriMatch) {
             call_user_func(self::$uriCallback[$uri]);
         } else {
             http_response_code(404);
@@ -72,3 +31,4 @@ class Route {
         }
     }
 }
+
